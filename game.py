67ceacc883fdcf6c39b1a2ed00d302e3b37e9619
Oktawski <oktawian.perlowski@gmail.com>
2022-12-18 22:14:06 +0100

    Added handling quit in game.py

--- game.py
+++ game.py
@@ -4,6 +4,8 @@ from romaji import Romaji
 class Game():
     def __init__(self):
         self.current_option: int = 0
+        self.points = 0
+        self.questions = 0
 
         self.options = {
             1: "Hiragana to Romanji",
@@ -41,11 +43,20 @@ class Game():
 
             words = input("Type above stuff in romaji:\n").split()
 
+            if "quit" in words:
+                self.quit()
+                return
+
             hiraganas_from_input = [self.romaji_to_hiragana[word] for word in words]
 
             is_winner = all([syllables[i] == hiraganas_from_input[i] for i in range(len(hiraganas_from_input))])
 
             correct_answer = " ".join([self.hiragana_to_romaji[i] for i in syllables])
+
+            self.questions += 1
+
+            if is_winner:
+                self.points += 1
 
             self.print_for_winner(is_winner, correct_answer)
         except KeyError:
@@ -64,3 +75,7 @@ class Game():
             print("Good job")
         else: 
             print("LOL no, try again. Should be: " + correct_answer)
+
+    def quit(self):
+        self.current_option = 3
+        print(f"\nYou have scored: {self.points} / {self.questions}\n")
